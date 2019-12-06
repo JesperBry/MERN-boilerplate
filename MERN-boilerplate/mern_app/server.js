ec854e06a5308fb3e1f72cc3d7080bcd8bcf07be
Jesper Brynildsen <jesper_bry@hotmail.com>
2019-12-06 15:49:25 +0100

    Updated dependencies + added useUnifiedTopology in mongoose.connect

--- MERN-boilerplate/mern_app/server.js
+++ MERN-boilerplate/mern_app/server.js
@@ -14,10 +14,7 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
